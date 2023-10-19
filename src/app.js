aad3a56d2540efff6cdf3fe6448280ebfdaccf43
unknown <rrdreaming@mail.ru>
2023-10-19 22:12:03 +0300

    :sparkles: ready project

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import * as THREE from 'three'
 import { addPass, useCamera, useGui, useRenderSize, useScene, useTick } from './render/init.js'
 // import postprocessing passes
-import { SavePass } from 'three/examples/jsm/postprocessing/SavePass.js'
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
-import { BlendShader } from 'three/examples/jsm/shaders/BlendShader.js'
-import { CopyShader } from 'three/examples/jsm/shaders/CopyShader.js'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
 
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
+
+import vertexPars from './shaders/vertex_pars.glsl'
+import vertexMain from './shaders/vertex_main.glsl'
+
+import fragmentPars from './shaders/fragment_pars.glsl'
+import fragmentMain from './shaders/fragment_main.glsl'
+
 
 const startApp = () => {
   const scene = useScene()
@@ -19,10 +23,10 @@ const startApp = () => {
   const MOTION_BLUR_AMOUNT = 0.725
 
   // lighting
-  const dirLight = new THREE.DirectionalLight('#ffffff', 0.75)
-  dirLight.position.set(5, 5, 5)
+  const dirLight = new THREE.DirectionalLight('#526cff', 0.6)
+  dirLight.position.set(2, 2, 2)
 
-  const ambientLight = new THREE.AmbientLight('#ffffff', 0.2)
+  const ambientLight = new THREE.AmbientLight('#4255ff', 0.5)
   scene.add(dirLight, ambientLight)
 
 
@@ -36,8 +40,33 @@ const startApp = () => {
 
 
   // meshes
-  const geometry = new THREE.IcosahedronGeometry(1, 5)
-  const material = new THREE.MeshStandardMaterial()
+  const geometry = new THREE.IcosahedronGeometry(1, 300)
+  const material = new THREE.MeshStandardMaterial({
+    onBeforeCompile: (shader) => {
+      // storing a reference to a shader object
+      material.userData.shader = shader;
+
+      // uniforms
+      shader.uniforms['uTime'] = { value: 0 }
+
+      const parsVertexString = `#include <displacementmap_pars_vertex>`
+      shader.vertexShader = shader.vertexShader.replace(parsVertexString,
+        parsVertexString + vertexPars)
+
+      const mainVertexString = `#include <displacementmap_vertex>`
+      shader.vertexShader = shader.vertexShader.replace(mainVertexString,
+        mainVertexString + vertexMain)
+
+      const mainFragmentString = `#include <normal_fragment_maps>`
+      shader.fragmentShader = shader.fragmentShader.replace(mainFragmentString,
+        mainFragmentString + fragmentMain)
+
+      const parsFragmentString = `#include <bumpmap_pars_fragment>`
+      shader.fragmentShader = shader.fragmentShader.replace(parsFragmentString,
+        parsFragmentString + fragmentPars)
+    },
+  })
+
 
   const ico = new THREE.Mesh(geometry, material)
   scene.add(ico)
@@ -56,31 +85,13 @@ const startApp = () => {
   cameraFolder.add(camera.position, 'z', 0, 10)
   cameraFolder.open()
 
-  // postprocessing
-  const renderTargetParameters = {
-    minFilter: THREE.LinearFilter,
-    magFilter: THREE.LinearFilter,
-    stencilBuffer: false,
-  }
-
-  // save pass
-  const savePass = new SavePass(new THREE.WebGLRenderTarget(width, height, renderTargetParameters))
-
-  // blend pass
-  const blendPass = new ShaderPass(BlendShader, 'tDiffuse1')
-  blendPass.uniforms['tDiffuse2'].value = savePass.renderTarget.texture
-  blendPass.uniforms['mixRatio'].value = MOTION_BLUR_AMOUNT
-
-  // output pass
-  const outputPass = new ShaderPass(CopyShader)
-  outputPass.renderToScreen = true
-
-  // adding passes to composer
-  addPass(blendPass)
-  addPass(savePass)
-  addPass(outputPass)
+  //postprocessing
+  addPass(new UnrealBloomPass(new THREE.Vector2(width, height), 0.7, 0.4, 0.4))
 
   useTick(({ timestamp, timeDiff }) => {
+    const time = timestamp / 10000;
+
+    material.userData.shader.uniforms['uTime'].value = time;
   })
 }
 
--- src/shaders/vertex_pars.glsl
+++ src/shaders/vertex_pars.glsl
@@ -1,12 +1,6 @@
 
 uniform float uTime;
-
-varying vec3 vPosition;
-varying vec3 vNormal;
-varying vec2 vUv;
 varying float vDisplacement;
-
-#define PI 3.14
 
 //	Classic Perlin 3D Noise 
 //	by Stefan Gustavson
@@ -96,27 +90,4 @@ float fit(float unscaled, float originalMin, float originalMax, float minAllowed
 
 float wave(vec3 position){
   return fit(smoothMod(position.y * 6.0, 1.0, 1.5), 0.35, 0.6, 0.0, 1.0);
-}
-
-
-void main() {
-  vec3 coords = normal;
-  coords.y += uTime;
-  vec3 noisePattern = vec3(noise(coords));
-  float pattern = wave(noisePattern);
-
-  // varyings
-
-  vPosition = position;
-  vNormal = normal;
-  vUv = uv;
-  vDisplacement = pattern;
-
-  float displacement = vDisplacement / 3.0;
-  
-  // MVP
-  vec3 newPosition = position + normal * (displacement);
-  vec4 modelViewPosition = modelViewMatrix * vec4( newPosition, 1.0 );
-  vec4 projectedPosition = projectionMatrix * modelViewPosition;
-  gl_Position = projectedPosition;
 }
